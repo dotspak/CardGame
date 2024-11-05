ae1de4a0f537946831f358f103a25805a5aa9ba1
dotspak <sparklesonic@gmail.com>
2024-11-05 11:05:59 -0400

    added images to booster tool (not displayed)

--- pectotools/src/card.gd
+++ pectotools/src/card.gd
@@ -4,3 +4,4 @@ class_name Card
 @export var name : String = "Greater Crow"
 @export_enum("Unit", "Off-Site", "Spell") var type : int = 0
 @export_enum("Basic", "Rare", "Super-Rare", "Divine-Rare") var rarity : int = 0
+@export var img : Texture2D
