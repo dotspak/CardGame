8c945ea8a58239b04f07a7438dd565187db8d0aa
dotspak <sparklesonic@gmail.com>
2025-10-02 21:57:58 -0300

    started work on click ui

--- simulator/pecto-tcg/PectoCard3D.gd
+++ simulator/pecto-tcg/PectoCard3D.gd
@@ -13,3 +13,12 @@ func set_front_face(_card : PectoCard) -> void:
 	card = _card
 	frontFace.call_deferred("add_child", card)
 	#card.get_child(0).scale = Vector2.ONE * 0.5
+
+func _click(_camera, event : InputEvent, _event_position, _normal, _shape_idx):
+	if event is InputEventMouseButton:
+		var button = event.button_index
+		var pressed = event.pressed
+		if button == 1 and pressed == true:
+			card_3d_mouse_down.emit()
+		elif button == 1 and pressed == false:
+			card_3d_mouse_up.emit()
